217d6f079b21250271d455bc52ca7b1983039798
PaoloVD <pvizconded@hotmail.com>
2023-10-23 14:43:09 -0500

    Creacion de Proyecto

---
+++ bookingrestaurante_Api/src/main/java/com/upao/restaurante/bookingrestaurante_Api/BookingrestauranteApiApplication.java
@@ -0,0 +1,13 @@
+package com.upao.restaurante.bookingrestaurante_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingrestauranteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingrestauranteApiApplication.class, args);
+	}
+
+}
---
+++ bookingrestaurante_Api/src/test/java/com/upao/restaurante/bookingrestaurante_Api/BookingrestauranteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.restaurante.bookingrestaurante_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingrestauranteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
