614d055587cc48dd7ae57c4ce02421c2e3dabd87
PaoloVD <pvizconded@hotmail.com>
2023-10-23 23:28:20 -0500

    Agregar interface RestaurantRepository

--- bookingrestaurante_Api/src/main/java/com/upao/restaurante/reservation/domain/Persistence/RestaurantRepository.java
+++ bookingrestaurante_Api/src/main/java/com/upao/restaurante/reservation/domain/Persistence/RestaurantRepository.java
@@ -1,4 +1,13 @@
 package com.upao.restaurante.reservation.domain.Persistence;
 
-public interface RestaurantRepository {
+import com.upao.restaurante.reservation.domain.Entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findByName(String name);
+
 }
